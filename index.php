a867bfd6eb4a43a437c702423ea77c40c57b58e8
Rob Monhemius <robmonhemius@gmail.com>
2021-09-16 06:20:01 +0100

    html-initial

--- index.php
+++ index.php
@@ -1,1 +1,73 @@
-<span>Oh no! I am an empty Theme.</span>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+  <title>Advanced WP Theme</title>
+</head>
+<body>
+  <header>
+    <div class="container">
+      <h1>Advanced WP Theme</h1>
+
+      <div class="h_right">
+        <form action="">
+          <input type="text" placeholder="Search">
+        </form>
+      
+      </div>
+    </div>
+  </header>
+  <nav class="nav main-nav">
+    <div class="container">
+      <ul>
+        <li><a href="./about.html">About</a></li>
+        <li><a href="./index.html">Home</a></li>
+        <li><a href="#">Services</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container content">
+    <div class="main block">
+      <article class='post'>
+        <h2>Blog Post</h2>
+        <p class="meta">Posted 06:00 on September 16 by admin.</p>
+        <p>Elit exercitation mollit adipisicing velit. Sunt commodo Lorem sint ea voluptate anim Lorem excepteur cillum velit non velit nisi. Enim magna laboris veniam aliquip commodo voluptate anim magna Lorem fugiat. Dolore minim ad qui nulla sint dolor id laboris aliquip. Voluptate magna adipisicing commodo et. Fugiat irure id labore minim reprehenderit aliqua quis minim sunt.</p>
+        <a href="#" class="button">Read More</a>
+      </article>
+
+      <article class='post'>
+        <h2>Blog Post</h2>
+        <p class="meta">Posted 06:00 on September 16 by admin.</p>
+        <p>Elit exercitation mollit adipisicing velit. Sunt commodo Lorem sint ea voluptate anim Lorem excepteur cillum velit non velit nisi. Enim magna laboris veniam aliquip commodo voluptate anim magna Lorem fugiat. Dolore minim ad qui nulla sint dolor id laboris aliquip. Voluptate magna adipisicing commodo et. Fugiat irure id labore minim reprehenderit aliqua quis minim sunt.</p>
+        <a href="#" class="button">Read More</a>
+      </article>
+
+      <article class='post'>
+        <h2>Blog Post</h2>
+        <p class="meta">Posted 06:00 on September 16 by admin.</p>
+        <p>Elit exercitation mollit adipisicing velit. Sunt commodo Lorem sint ea voluptate anim Lorem excepteur cillum velit non velit nisi. Enim magna laboris veniam aliquip commodo voluptate anim magna Lorem fugiat. Dolore minim ad qui nulla sint dolor id laboris aliquip. Voluptate magna adipisicing commodo et. Fugiat irure id labore minim reprehenderit aliqua quis minim sunt.</p>
+        <a href="#" class="button">Read More</a>
+      </article>
+    </div>
+  </div>
+
+  <footer class="main-footer">
+    <div class="container">
+      <div class="f_left">
+        <p>&amp;copy; 2021 - Advanced WP Theme</p>
+      </div>
+      <div class="f_right">
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="#">Services</a></li>
+      </ul>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
+<?php
