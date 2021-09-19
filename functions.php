b5013edf6a0649dbc8a22b0d284d7b845329607c
Rob Monhemius <robmonhemius@gmail.com>
2021-09-19 08:49:40 +0100

    subnavigation

--- functions.php
+++ functions.php
@@ -16,3 +16,20 @@ function theme_excerpt_length() {
   return 25;
 }
 add_filter( 'excerpt_length', 'theme_excerpt_length' );
+
+
+// FUNCTIONS
+
+function get_top_parent( $post ) {
+  if( $post->post_parent ) {
+    $ancestors = get_post_ancestors( $post->ID );
+    $result = $ancestors[0];
+  } else {
+    $result = $post->ID;
+  }
+  return $result;
+}
+
+function page_is_parent( $post ) {
+  return count( get_pages( ['child_of' => $post->ID ] ) );
+}
--- page.php
+++ page.php
@@ -4,6 +4,20 @@
       <?php 
       if( have_posts() ):
         while( have_posts() ): the_post();
+        if(page_is_parent( $post ) || $post->post_parent > 0):
+          print('<nav class="nav sub-nav">');
+          printf(
+            '<span class="parent-link"><a href="%s">%s</a></span>',
+            get_the_permalink( get_top_parent( $post ) ),
+            get_the_title( get_top_parent( $post ) )
+          );
+          wp_list_pages([
+            'child_of' => get_top_parent( $post ),
+            'title_li' => ''
+          ]);
+          print('</nav>');
+          print('<div class="clr"></div>');
+        endif;
           print('<article>');
           the_title( '<h1>', '</h1>' );
           the_content();
